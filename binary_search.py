520430a9bb9e8ae30c18212671a7bfe88bd12301
Javier Gonzalez <Oscar.Gonzalez.24@cmc.edu>
2023-02-21 20:18:23 -0800

    try 1

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,5 @@
 #!/bin/python3
 '''
-JOKE: There are 2 hard problems in computer science: cache invalidation, naming things, and off-by-1 errors.
-
 It's really easy to have off-by-1 errors in these problems.
 Pay very close attention to your list indexes and your < vs <= operators.
 '''
@@ -11,115 +9,131 @@ def find_smallest_positive(xs):
     Assume that xs is a list of numbers sorted from LOWEST to HIGHEST.
     Find the index of the smallest positive number.
     If no such index exists, return `None`.
-
-    HINT: 
-    This is essentially the binary search algorithm from class,
-    but you're always searching for 0.
-
-    APPLICATION:
-    This is a classic question for technical interviews.
-
-    >>> find_smallest_positive([-3, -2, -1, 0, 1, 2, 3])
-    4
-    >>> find_smallest_positive([1, 2, 3])
-    0
-    >>> find_smallest_positive([-3, -2, -1]) is None
-    True
     '''
+    def find_smallest_positive_helper(xs, low, high):
+        if low > high:
+            return None
+
+        mid = (low + high) // 2
+
+        if xs[mid] > 0:
+            if mid == 0 or xs[mid - 1] <= 0:
+                return mid
+            else:
+                return find_smallest_positive_helper(xs, low, mid - 1)
+        else:
+            return find_smallest_positive_helper(xs, mid + 1, high)
+
+    return find_smallest_positive_helper(xs, 0, len(xs) - 1)
 
 
 def count_repeats(xs, x):
-    '''
+    """
     Assume that xs is a list of numbers sorted from HIGHEST to LOWEST,
     and that x is a number.
     Calculate the number of times that x occurs in xs.
+    """
+    def leftmost(xs, x, lo, hi):
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if xs[mid] == x:
+                if mid == 0 or xs[mid - 1] != x:
+                    return mid
+                hi = mid - 1
+            elif xs[mid] < x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
 
-    HINT: 
-    Use the following three step procedure:
-        1) use binary search to find the lowest index with a value >= x
-        2) use binary search to find the lowest index with a value < x
-        3) return the difference between step 1 and 2
-    I highly recommend creating stand-alone functions for steps 1 and 2,
-    and write your own doctests for these functions.
-    Then, once you're sure these functions work independently,
-    completing step 3 will be easy.
+    def rightmost(xs, x, lo, hi):
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if xs[mid] == x:
+                if mid == len(xs) - 1 or xs[mid + 1] != x:
+                    return mid
+                lo = mid + 1
+            elif xs[mid] < x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
+    left = leftmost(xs, x, 0, len(xs) - 1)
+    if left == -1:
+        return 0
+    right = rightmost(xs, x, left, len(xs) - 1)
+    return right - left + 1
 
-    APPLICATION:
-    This is a classic question for technical interviews.
 
-    >>> count_repeats([5, 4, 3, 3, 3, 3, 3, 3, 3, 2, 1], 3)
-    7
-    >>> count_repeats([3, 2, 1], 4)
-    0
-    '''
+def count(xs, x):
+    """
+    Assume that xs is a list of numbers sorted from HIGHEST to LOWEST,
+    and that x is a number.
+    Calculate the number of times that x occurs in xs.
+    """
+    def leftmost(xs, x, lo, hi):
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if xs[mid] == x:
+                if mid == 0 or xs[mid - 1] != x:
+                    return mid
+                hi = mid - 1
+            elif xs[mid] < x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
+
+    def rightmost(xs, x, lo, hi):
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if xs[mid] == x:
+                if mid == len(xs) - 1 or xs[mid + 1] != x:
+                    return mid
+                lo = mid + 1
+            elif xs[mid] < x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
+    left = leftmost(xs, x, 0, len(xs) - 1)
+    if left == -1:
+        return 0
+    right = rightmost(xs, x, left, len(xs) - 1)
+    return right - left + 1
 
 
 def argmin(f, lo, hi, epsilon=1e-3):
-    '''
-    Assumes that f is an input function that takes a float as input and returns a float with a unique global minimum,
-    and that lo and hi are both floats satisfying lo < hi.
-    Returns a number that is within epsilon of the value that minimizes f(x) over the interval [lo,hi]
+    if hi - lo < epsilon:
+        return (lo + hi) / 2
 
-    HINT:
-    The basic algorithm is:
-        1) The base case is when hi-lo < epsilon
-        2) For each recursive call:
-            a) select two points m1 and m2 that are between lo and hi
-            b) one of the 4 points (lo,m1,m2,hi) must be the smallest;
-               depending on which one is the smallest, 
-               you recursively call your function on the interval [lo,m2] or [m1,hi]
+    m1 = lo + (hi - lo) / 3
+    m2 = hi - (hi - lo) / 3
 
-    APPLICATION:
-    Essentially all data mining algorithms are just this argmin implementation in disguise.
-    If you go on to take the data mining class (CS145/MATH166),
-    we will spend a lot of time talking about different f functions that can be minimized and their applications.
-    But the actual minimization code will all be a variant of this binary search.
+    if f(m1) < f(m2):
+        return argmin(f, lo, m2, epsilon)
+    else:
+        return argmin(f, m1, hi, epsilon)
 
-    WARNING:
-    The doctests below are not intended to pass on your code,
-    and are only given so that you have an example of what the output should look like.
-    Your output numbers are likely to be slightly different due to minor implementation details.
-    Writing tests for code that uses floating point numbers is notoriously difficult.
-    See the pytests for correct examples.
-
-    >>> argmin(lambda x: (x-5)**2, -20, 20)
-    5.000040370009773
-    >>> argmin(lambda x: (x-5)**2, -20, 0)
-    -0.00016935087808430278
-    '''
-
-
-################################################################################
-# the functions below are extra credit
-################################################################################
 
 def find_boundaries(f):
-    '''
-    Returns a tuple (lo,hi).
-    If f is a convex function, then the minimum is guaranteed to be between lo and hi.
-    This function is useful for initializing argmin.
+    lo = -1
+    hi = 1
 
-    HINT:
-    Begin with initial values lo=-1, hi=1.
-    Let mid = (lo+hi)/2
-    if f(lo) > f(mid):
-        recurse with lo*=2
-    elif f(hi) < f(mid):
-        recurse with hi*=2
-    else:
-        you're done; return lo,hi
-    '''
+    while f(hi) < f(lo):
+        lo = hi
+        hi *= 2
+
+    while hi - lo > 1e-6:
+        mid = (lo + hi) / 2
+        if f(mid) < f(mid + 1e-6):
+            hi = mid
+        else:
+            lo = mid
+
+    return lo, hi
 
 
 def argmin_simple(f, epsilon=1e-3):
-    '''
-    This function is like argmin, but it internally uses the find_boundaries function so that
-    you do not need to specify lo and hi.
-
-    NOTE:
-    There is nothing to implement for this function.
-    If you implement the find_boundaries function correctly,
-    then this function will work correctly too.
-    '''
     lo, hi = find_boundaries(f)
     return argmin(f, lo, hi, epsilon)
