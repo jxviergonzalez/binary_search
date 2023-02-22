30001387655209a9da5e0de77a6b7c1acd53c3a2
Javier Gonzalez <Oscar.Gonzalez.24@cmc.edu>
2023-02-21 20:20:04 -0800

    linter

--- binary_search.py
+++ binary_search.py
@@ -4,6 +4,7 @@ It's really easy to have off-by-1 errors in these problems.
 Pay very close attention to your list indexes and your < vs <= operators.
 '''
 
+
 def find_smallest_positive(xs):
     '''
     Assume that xs is a list of numbers sorted from LOWEST to HIGHEST.
